643e9c70808fdcb02ee2a2fd3504f5992c1372ea
fivesixkilo <hart.crompton@gmail.com>
2017-07-16 12:07:41 -0600

    Testing email form submission

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
 
 
 //E-mail address. Enter your email
-define("__TO__", "youremail@domain.com");
+define("__TO__", "hart.crompton@gmail.com");
 
 //Success message
 define('__SUCCESS_MESSAGE__', "Your message has been sent. We will reply soon. Thank you!");
